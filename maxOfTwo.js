48dc1b92cce0d60ed047d112224bb4d42c73cc62
Donovan McCollum <donomcc@gmail.com>
2022-07-05 19:30:33 -0500

    Completed problem

--- maxOfTwo.js
+++ maxOfTwo.js
@@ -3,3 +3,12 @@ let y = 2
 
 // Write your code below this line.
 
+if (x > y){
+    console.log(x);
+}
+else if (x < y){
+    console.log(y);
+}
+else if (x === y){
+    console.log("The values are identical.");
+}
